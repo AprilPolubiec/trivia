9c59860a98d5e8544832dcdd50cd84c28ee29627
April Polubiec <aprilpolubiec@gmail.com>
2021-01-23 08:39:49 -0600

    refactor navigate function

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,19 +1,21 @@
 import Landing from "./pages/landing";
 import Lobby from "./pages/lobby";
+import Question from "./pages/question";
 
 import { CONTAINER_ID } from "./constants";
 
 const routes = {
   landing: Landing,
   lobby: Lobby,
+  question: Question,
 };
 
 const container = document.getElementById(CONTAINER_ID);
 
 export const navigate = (destination, params) => {
-  if (container.childNodes[0]) {
-    container.replaceChild(routes[destination](params), container.childNodes[0]);
-  } else {
-    container.appendChild(routes[destination](params))
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
+
+  container.appendChild(routes[destination](params));
 };
